26ffab7a8f8b8c4d7d6a6531d12e8c248e634e80
shaowei2019 <15090825090@163.com>
2019-04-13 10:26:05 +0800

    数据库升级

--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -1,17 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
 from simpledu.config import configs
 from simpledu.models import db
+
 def register_blueprints(app):
     from .handlers import front,course,admin
     app.register_blueprint(front)
     app.register_blueprint(course)
     app.register_blueprint(admin)
+
 def create_app(config):
 
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
 
     db.init_app(app)
+    Migrate(app,db)
     register_blueprints(app)
     return app
 
